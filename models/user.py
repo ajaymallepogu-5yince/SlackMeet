a3253a8199ad2cb1148ce840fd56c5f82403091a
Ajay <ajay.mallepogu@5yinc.com>
2026-05-06 13:16:50 +0530

    remove duplicate mentions in the user code file

--- models/user.py
+++ models/user.py
@@ -6,8 +6,8 @@ class UserToken(Base):
     __tablename__ = "user_tokens"
 
     user_id = Column(String, primary_key=True, index=True)
-    access_token = Column(String)
-    refresh_token = Column(String)
+    access_token = Column(Text)
+    refresh_token = Column(Text)
     token_uri = Column(Text)
     client_id = Column(Text)
     client_secret = Column(Text)
